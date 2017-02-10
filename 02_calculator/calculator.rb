73e193dfe6fe7e7d5313db74afc5c907e67a7184
Joshua Dietz <dylan817@live.com>
2017-02-10 01:21:57 -0600

    Solve third problem, working on bonus

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,48 @@
 #write your code here
+def add num1, num2
+  num1 + num2
+end
+def subtract num1, num2
+  num1 - num2
+end
+
+def sum nums
+  total = 0
+  for each in nums do
+  total += each
+  end
+  if total == nil
+    total = 0
+  end
+  total
+end
+
+def multiply (num1, *rest)
+  for each in rest do
+    total = (num1 * each)
+    num1 = total
+  end
+total
+end
+
+def power num1, num2
+  num1 ** num2
+
+end
+#def factorial num
+  #if num != 0
+    #while num > 0
+      #if num == 1
+        #total = num * (num)
+        #num -= 1
+      #else
+        #total = num * (num -1)
+        #num -= 1
+      #end
+
+    #end
+  #else
+    #total = 1
+  #end
+  #total
+#end
