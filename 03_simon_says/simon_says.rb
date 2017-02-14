13dbc0d644a7064664e469e485f7bd6c127200f6
Joshua Dietz <dylan817@live.com>
2017-02-14 01:13:04 -0600

    Start solving fourth problem

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,42 @@
-#write your code here
+#simon says echo should echo hello
+def echo input
+  echo = input
+end
+def shout input
+  shout = input.upcase
+end
+def repeat input, amount = 1
+  repeat = input
+  amount -= 1
+  if amount > 1
+   amount.times do
+    repeat +=  " " + input
+    end
+  else
+    repeat += " " + input
+  end
+  repeat
+end
+
+def start_of_word input, num
+  cutoff = num - 1
+  word = input[0..cutoff]
+  word
+end
+
+def first_word input
+  input = input.split(' ')
+  input[0]
+end
+
+def titleize input
+  little_words = ['a', 'an', 'the','for', 'and', 'nor','but', 'or', 'yet', 'so']
+  words = input.split.map(&:capitalize)
+
+  for each in words
+    if little_words.include?(each)
+      each.downcase
+    end
+  end
+  words = words.join(" ")
+end
