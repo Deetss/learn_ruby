e26211d21c36dfdeff9cfd30f0f0af1f37c5063e
Joshua Dietz <dylan817@live.com>
2017-02-24 14:15:56 -0600

    Solved bonus for problem 5

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,34 +1,47 @@
 #write your code here
 def translate input
   vowels = ['a','e','i','o','u']
-  word_split = input.downcase.split(' ')
+  word_split = input.split(' ')
 
   for word in word_split
+    #check capitalization
+    word_cap = false
+    if word == word.capitalize
+      word_cap = true
+    end
+    #check if first letter is a vowel
     if vowels.include?(word[0])
     word.replace(word + "ay")
     else
+      #check first 3 letters for consonants
       if !vowels.include?(word[1]) && !vowels.include?(word[2])
-        not_first = word.slice(3..-1)
+        ending = word.slice(3..-1)
         first_three = word.slice(0..2)
-        word.replace(not_first + first_three + 'ay')
+        word.replace(ending + first_three + 'ay')
       elsif word.slice(1..2) == 'qu' && !vowels.include?(word[0])
-        not_first = word.slice(3..-1)
+        ending = word.slice(3..-1)
         first_three = word.slice(0..2)
-        word.replace(not_first + first_three + 'ay')
+        word.replace(ending + first_three + 'ay')
       elsif word.slice(0..1) == 'qu'
-        not_first = word.slice(2..-1)
+        ending = word.slice(2..-1)
         first_two = word.slice(0..1)
-        word.replace(not_first + first_two + 'ay')
+        word.replace(ending + first_two + 'ay')
       elsif !vowels.include?(word[1])
-        not_first = word.slice(2..-1)
+        ending = word.slice(2..-1)
         first_two = word.slice(0..1)
-        word.replace(not_first + first_two + 'ay')
+        word.replace(ending + first_two + 'ay')
       else
-        not_first = word.slice(1..-1)
+        ending = word.slice(1..-1)
         first = word.slice(0)
-        word.replace(not_first + first + 'ay')
+        word.replace(ending + first + 'ay')
       end
     end
+    #change capitalization of the word if it was capitalized to begin with
+    if word_cap
+      word.downcase
+      word.capitalize!
+    end
   end
+
   word_split.join(' ')
 end
