ec87bd13e942aa849ffdde85b2eb5e114cd44691
Joshua Dietz <dylan817@live.com>
2017-02-28 23:03:10 -0600

    Solved final problem

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,39 @@
 class Timer
-  #write your code here
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def seconds=(num)
+    @seconds = num
+  end
+
+  def time_string
+    secs = @seconds
+    string = '00:00:00'
+    string = string.split(':')
+    hours = @seconds / 3600
+    hours_rem = @seconds % 3600
+    mins = hours_rem / 60
+    mins_rem = hours_rem % 60
+
+    if secs == 0
+      string.join(":")
+    elsif secs < 60
+      string[2] = "%02d" % [mins_rem]
+    elsif secs < 3600
+      string[1] = "%02d" % [mins]
+      string[2] = "%02d" % [mins_rem]
+    else
+      string[0] = "%02d" % [hours]
+      string[1] = "%02d" % [mins]
+      string[2] = "%02d" % [mins_rem]
+    end
+
+    string.join(":")
+
+  end
 end
